a3e17212217959649068c6dd9407d306d57fc8fb
Renaud Gervais <renaud.gervais@gmail.com>
2015-05-30 13:16:18 +0100

    Simple mindmap to brainstorm an outline

---
+++ OutlineStorm.mm
@@ -0,0 +1,78 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Thesis" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1432984803602"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="Calibri" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#666666">
+<font SIZE="8" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6"/>
+<node TEXT="Title" POSITION="right" ID="ID_349062740" CREATED="1432984828407" MODIFIED="1432984829812">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Creative Coding on Object" POSITION="right" ID="ID_455849311" CREATED="1432984804193" MODIFIED="1432984814692">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="Introduction" POSITION="right" ID="ID_5485283" CREATED="1432984834744" MODIFIED="1432984837420">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Related Works" POSITION="right" ID="ID_471341881" CREATED="1432984839904" MODIFIED="1432984843556">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Interacting with SAR" POSITION="right" ID="ID_474983233" CREATED="1432984884894" MODIFIED="1432984887372">
+<edge COLOR="#00ffff"/>
+<node TEXT="CurSAR" ID="ID_1255138742" CREATED="1432984891504" MODIFIED="1432984894900"/>
+<node TEXT="Tangible Viewports" ID="ID_571005933" CREATED="1432984895224" MODIFIED="1432984901499"/>
+</node>
+<node TEXT="Tangible Out of Body Experiences" POSITION="right" ID="ID_1524952081" CREATED="1432984923079" MODIFIED="1432984934044">
+<edge COLOR="#ffff00"/>
+<node TEXT="Teegi" ID="ID_729640759" CREATED="1432984937329" MODIFIED="1432984939148"/>
+<node TEXT="TOBE" ID="ID_693643857" CREATED="1432984939655" MODIFIED="1432984943940"/>
+</node>
+</node>
+</map>
