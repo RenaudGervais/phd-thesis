a1794b3e9a18654ad70fe03668192be86e999b22
Renaud Gervais <renaud.gervais@gmail.com>
2015-06-01 09:18:08 +0100

    Adding some info on the TOBE part

--- OutlineStorm.mm
+++ OutlineStorm.mm
@@ -8,7 +8,9 @@
 <stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
 <font NAME="Calibri" SIZE="10" BOLD="false" ITALIC="false"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#666666">
+<font SIZE="8" ITALIC="true"/>
+</stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#666666">
 <font SIZE="8" ITALIC="true"/>
 </stylenode>
@@ -35,10 +37,10 @@
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
 <font SIZE="18"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#3399ff">
 <font SIZE="16"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00cc33">
 <font SIZE="14"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
@@ -52,14 +54,20 @@
 </map_styles>
 </hook>
 <hook NAME="AutomaticEdgeColor" COUNTER="7"/>
-<node TEXT="Title" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_349062740" CREATED="1432984828407" MODIFIED="1432988443823">
+<node TEXT="Title" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_349062740" CREATED="1432984828407" MODIFIED="1433146012840">
 <edge COLOR="#0000ff"/>
+<node TEXT="Grasping the Intangible" ID="ID_1365235857" CREATED="1433145779197" MODIFIED="1433146238697"/>
+<node TEXT="Getting Out of Flatland" ID="ID_168816496" CREATED="1433146002085" MODIFIED="1433146008368"/>
 </node>
-<node TEXT="Introduction" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_5485283" CREATED="1432984834744" MODIFIED="1432988443825">
+<node TEXT="Introduction" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_5485283" CREATED="1432984834744" MODIFIED="1433146012846">
 <edge COLOR="#00ff00"/>
+<node TEXT="Humane representation of thoughts" ID="ID_1870474390" CREATED="1433141710712" MODIFIED="1433141945347" LINK="https://vimeo.com/115154289"/>
 <node TEXT="Creative Coding on Object" ID="ID_455849311" CREATED="1432984804193" MODIFIED="1432988409000">
 <node TEXT="Objects will be imbued with computational power" ID="ID_560049132" CREATED="1432990665268" MODIFIED="1432990678753"/>
-<node TEXT="Computational Objects as SDKs of themselves" ID="ID_759474995" CREATED="1432991166605" MODIFIED="1432991179970"/>
+<node TEXT="Computational Objects as SDKs of themselves" ID="ID_759474995" CREATED="1432991166605" MODIFIED="1432991179970">
+<node TEXT="3D model of themselves" ID="ID_1206377662" CREATED="1433138407428" MODIFIED="1433138415994"/>
+<node TEXT="Knows about possible inputs and outputs" ID="ID_840645801" CREATED="1433138417748" MODIFIED="1433138430737"/>
+</node>
 <node TEXT="How to" ID="ID_1476587554" CREATED="1432991180454" MODIFIED="1432991191449">
 <node TEXT="Customize" ID="ID_1315025088" CREATED="1432991191942" MODIFIED="1432991349667">
 <hook NAME="FirstGroupNode"/>
@@ -71,7 +79,11 @@
 </node>
 </node>
 <node TEXT="Affordances as a propriety of the object that can be included in the SDK" ID="ID_445287555" CREATED="1432991405949" MODIFIED="1432991425602"/>
+<node TEXT="Adding affordances by adding controllers" ID="ID_614123133" CREATED="1433138896940" MODIFIED="1433138931498">
+<node TEXT="Could be as easy as with littleBits" ID="ID_407482777" CREATED="1433138932199" MODIFIED="1433138958908" LINK="http://littlebits.cc"/>
 </node>
+</node>
+<node TEXT="Ubiquitous Computing" ID="ID_677564017" CREATED="1432991467574" MODIFIED="1432991470874"/>
 <node TEXT="OUI" ID="ID_1020269710" CREATED="1432988462972" MODIFIED="1432988463726">
 <node TEXT="Hypercontext" ID="ID_1692703247" CREATED="1432988510835" MODIFIED="1432988517907">
 <font BOLD="true"/>
@@ -83,22 +95,23 @@
 <node TEXT="Technological mean to emulate screens everywhere" ID="ID_1595535281" CREATED="1432988542652" MODIFIED="1432988552823"/>
 </node>
 <node TEXT="TUI" ID="ID_573573029" CREATED="1432988474490" MODIFIED="1432988475727"/>
-<node TEXT="Ubiquitous Computing" ID="ID_677564017" CREATED="1432991467574" MODIFIED="1432991470874"/>
 </node>
-<node TEXT="Related Works" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_471341881" CREATED="1432984839904" MODIFIED="1432988443826">
+<node TEXT="Related Works" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_471341881" CREATED="1432984839904" MODIFIED="1433146012857">
 <edge COLOR="#ff00ff"/>
 <node TEXT="SAR" ID="ID_1861700228" CREATED="1432988461291" MODIFIED="1432988462263"/>
 <node TEXT="OUI" ID="ID_442662300" CREATED="1432988462972" MODIFIED="1432988463726"/>
 <node TEXT="TUI" ID="ID_1394497496" CREATED="1432988474490" MODIFIED="1432988475727"/>
 </node>
-<node TEXT="Interacting with SAR" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_474983233" CREATED="1432984884894" MODIFIED="1432988443827">
+<node TEXT="Interacting with SAR" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_474983233" CREATED="1432984884894" MODIFIED="1433146012863">
 <edge COLOR="#00ffff"/>
 <node TEXT="Settings" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_418879808" CREATED="1432992318262" MODIFIED="1432992402048">
 <node TEXT="SAR is a medium that is very expressive" ID="ID_1048580932" CREATED="1432992256599" MODIFIED="1432992263858"/>
 <node TEXT="Put a new emphasis on the physical world compared to video see through because no middle barrier" ID="ID_1169448401" CREATED="1432992264111" MODIFIED="1432992309979"/>
 </node>
 <node TEXT="Problem" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1936534183" CREATED="1432992248271" MODIFIED="1432992401353">
-<node TEXT="How to interact with a medium that is virtual but that is hosted on reachable and manipulable physical objects?" ID="ID_1567237923" CREATED="1432992330047" MODIFIED="1432992365708"/>
+<node TEXT="How to interact with a medium that is virtual but that is hosted on reachable and manipulable physical objects?" ID="ID_1567237923" CREATED="1432992330047" MODIFIED="1433145730790">
+<font BOLD="true"/>
+</node>
 </node>
 <node TEXT="CurSAR" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1255138742" CREATED="1432984891504" MODIFIED="1432992397724">
 <node TEXT="Study that compare the pointing performance of 2D input devices between a Screen and SAR setups" ID="ID_844891205" CREATED="1432991484983" MODIFIED="1432991516481"/>
@@ -109,14 +122,46 @@
 <node TEXT="Tangible equivalent of a 3D viewport" ID="ID_363248684" CREATED="1432991610741" MODIFIED="1432991618474"/>
 </node>
 </node>
-<node TEXT="Tangible Out of Body Experiences" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_1524952081" CREATED="1432984923079" MODIFIED="1432988443828">
-<edge COLOR="#ffff00"/>
-<node TEXT="Settings" ID="ID_1460414111" CREATED="1432992412423" MODIFIED="1432992414738"/>
-<node TEXT="Problem" ID="ID_813031516" CREATED="1432992415023" MODIFIED="1432992416778"/>
-<node TEXT="Teegi" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_729640759" CREATED="1432984937329" MODIFIED="1432992422824"/>
-<node TEXT="TOBE" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_693643857" CREATED="1432984939655" MODIFIED="1432992422569"/>
+<node TEXT="Tangible Out of Body Experiences" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1524952081" CREATED="1432984923079" MODIFIED="1433146012867">
+<edge COLOR="#ff0000"/>
+<node TEXT="Settings" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_1460414111" CREATED="1432992412423" MODIFIED="1433145010315">
+<node TEXT="More and more physiological sensors are made available" ID="ID_827210172" CREATED="1433145020435" MODIFIED="1433145045199"/>
+<node TEXT="Mainly targeted at sports" ID="ID_1477618524" CREATED="1433145045466" MODIFIED="1433145065527"/>
+<node TEXT="Visualizations using standard charts /w mobile apps" ID="ID_62648870" CREATED="1433145065755" MODIFIED="1433145084014"/>
 </node>
-<node TEXT="Conclusion" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_157081400" CREATED="1432988424107" MODIFIED="1432988443828">
+<node TEXT="Problem" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_813031516" CREATED="1432992415023" MODIFIED="1433145009990">
+<node TEXT="What is happening inside our bodies are not only related to sports" ID="ID_1553023449" CREATED="1433145086500" MODIFIED="1433145149911"/>
+<node TEXT="Some components are difficult to visualize and grasp because it is abstract" ID="ID_365749362" CREATED="1433145150124" MODIFIED="1433145277082"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      e.g. Stress, attention
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="Wellbeing incompatible with metrics" ID="ID_1980098322" CREATED="1433145579420" MODIFIED="1433145608072">
+<node TEXT="Metrics foster competition and comparison" ID="ID_717627727" CREATED="1433145644460" MODIFIED="1433145657367"/>
+<node TEXT="Wellbeing is more about what you feel, not how you compare to others or oneself" ID="ID_296307168" CREATED="1433145661453" MODIFIED="1433145683703"/>
+</node>
+<node TEXT="How to simplify the interaction and exploration of our own physiology w/o relying on explicitely numerical displays?" ID="ID_1824786279" CREATED="1433145440108" MODIFIED="1433145732611">
+<font BOLD="true"/>
+</node>
+</node>
+<node TEXT="Teegi" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_729640759" CREATED="1432984937329" MODIFIED="1432992422824">
+<node TEXT="Is a tangible interface enabling users to explore EEG signals of their brain in real-time reprojected onto the scalp of a doll" ID="ID_1244149550" CREATED="1418979282731" MODIFIED="1418979326074"/>
+</node>
+<node TEXT="TOBE" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_693643857" CREATED="1432984939655" MODIFIED="1432992422569">
+<node TEXT="Is a platform that enables the exploration and representation of real-time biosignals through tangible avatars" ID="ID_1851214960" CREATED="1418979959139" MODIFIED="1433144554216"/>
+</node>
+</node>
+<node TEXT="Conclusion" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_157081400" CREATED="1432988424107" MODIFIED="1433146012874">
 <edge COLOR="#7c0000"/>
 </node>
 </node>
